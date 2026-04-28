c9483ac9d2acb242aa08614f441d09268e007ab1
Herrick <herrickfabian21@gmail.com>
2026-04-28 23:25:12 +0700

    buat fitur api qris untuk pembayaran

---
+++ app/api/qris/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { orderId, nominal } = body;
+
+    console.log(`Request QRIS pesanan: ${orderId}`);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    return NextResponse.json({
+      sukses: true,
+      pesan: "Berhasil generate QRIS",
+      orderId: orderId,
+      nominal: nominal,
+      urlGambarQris: `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=Pembayaran_Parkir_Dummy_Untuk_${orderId}`
+    }, { status: 200 });
+
+  } catch (error) {
+    return NextResponse.json({ 
+      sukses: false, 
+      pesan: "Error bos" 
+    }, { status: 500 });
+  }
+}
